6d25bbf3afbf778a72670954dd82e1561b2004d5
Arjit.Agrawal <arjit.agrawal@kwa-analytics.com>
2018-03-15 14:48:41 +0530

    replaced Jhon with James

--- src/UserClient.java
+++ src/UserClient.java
@@ -9,7 +9,7 @@ public class UserClient {
 
 	public User getUser() {
 		  return new
-		    User.UserBuilder("Jhon", "Doe")
+		    User.UserBuilder("James", "Doe")
 		    .age(30)
 		    .phone("1234567")
 		    .address("Fake address 1234")
